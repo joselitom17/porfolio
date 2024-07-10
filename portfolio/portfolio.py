fa10fd3f1ec11670364d744f38232600b4189003
joselitom17 <josemartalpmusica2020@gmail.com>
2024-07-10 18:03:04 +0200

    Portfolio Template

---
+++ portfolio/portfolio.py
@@ -0,0 +1,61 @@
+import reflex as rx
+from portfolio import data
+from portfolio.styles.styles import BASE_STYLE, MAX_WIDTH, STYLESHEETS, EmSize, Size
+from portfolio.views.about import about
+from portfolio.views.extra import extra
+from portfolio.views.footer import footer
+from portfolio.views.header import header
+from portfolio.views.info import info
+from portfolio.views.tech_stack import tech_stack
+
+DATA = data.data
+
+
+def index() -> rx.Component:
+    return rx.center(
+        # rx.theme_panel(),
+        rx.vstack(
+            header(DATA),
+            about(DATA.about),
+            rx.divider(),
+            tech_stack(DATA.technologies),
+            info("Experiencia", DATA.experience),
+            info("Proyectos Destacados", DATA.projects),
+            info("Formación", DATA.training),
+            extra(DATA.extras),
+            rx.divider(),
+            footer(DATA.media),
+            spacing=Size.MEDIUM.value,
+            padding_x=EmSize.MEDIUM.value,
+            padding_y=EmSize.BIG.value,
+            max_width=MAX_WIDTH,
+            width="100%"
+        )
+    )
+
+
+app = rx.App(
+    stylesheets=STYLESHEETS,
+    style=BASE_STYLE,
+    theme=rx.theme(
+        appearance="dark",
+        accent_color="sky",
+        radius="full"
+    )
+)
+
+title = DATA.title
+description = DATA.description
+image = DATA.image
+
+app.add_page(
+    index,
+    title=title,
+    description=description,
+    image=image,
+    meta=[
+        {"name": "og:title", "content": title},
+        {"name": "og:description", "content": description},
+        {"name": "og:image", "content": image}
+    ]
+)
